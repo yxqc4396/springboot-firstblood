a250ec001ccb1b691b3b0c68351408a4c4bef9b2
wangj <wj970307>
2018-03-16 12:19:41 +0800

    测试提交

--- src/main/java/com/SpringbootFirstbloodApplication.java
+++ src/main/java/com/SpringbootFirstbloodApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootFirstbloodApplication {
+	/**
+	 * 测试提交
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootFirstbloodApplication.class, args);
 	}
